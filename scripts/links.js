0ba65d8205322b09569c2a8d017008938185521b
byui-titus <Titusrayz33@gmail.com>
2024-03-05 04:06:43 -0700

    link

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,27 @@
 const baseURL = "https://byui-titus.github.io/wdd230/";
-const linksURL = "https://alansanchezzamora.github.io/wdd230/data/links.json";
+const linksURL = "https://byui-titus.github.io/wdd230/data/links.json";
 const cards = document.querySelector("#cards");
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  }
+
+  const displayLinks = (weeks) => {
+  weeks.foreach((week) => {
+    let card = document.createElement("section");
+    let week = document.createElement("p");
+    let activity = document.createElement("a");
+
+    week.textcontent = `${week.lesson}`;
+    activity.setAttribute("href", week.url);
+    activity.setAttribute("");
+
+    card.appendChild(week);
+    card.appendChild(activity);
+
+    cards.appendChild(card);
+  });
+};
+getLinks();
