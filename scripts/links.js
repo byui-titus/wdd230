3abd9a4d5456c184a0b9cde9777cff34414214e8
byui-titus <Titusrayz33@gmail.com>
2024-03-05 02:49:48 -0700

    dat

---
+++ scripts/links.js
@@ -0,0 +1,3 @@
+const baseURL = "https://byui-titus.github.io/wdd230/";
+const linksURL = "https://alansanchezzamora.github.io/wdd230/data/links.json";
+const cards = document.querySelector("#cards");
