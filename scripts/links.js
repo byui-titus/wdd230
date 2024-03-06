5f64499a41dbf2be9a02bd3412b62907b225b1f2
byui-titus <Titusrayz33@gmail.com>
2024-03-06 05:50:12 -0700

    lik

--- scripts/links.js
+++ scripts/links.js
@@ -1,46 +1,27 @@
-const baseURL = "https://byui-titus.github.io/wdd230/";
-const linksURL = "https://byui-titus.github.io/wdd230/data/links.json";
-
+const baseURL = "https://github.com/byui-titus/wdd230";
+const linksURL = "https://github.com/byui-titus/wdd230/blob/main/links/links.json";
+const cards = document.querySelector("#cards");
 
 async function getLinks() {
-  try {
-      const response = await fetch(linksURL);
-      const data = await response.json();
-      console.log(data); // Test JSON data
-      displayLinks(data); // Call function to display links
-  } catch (error) {
-      console.error('Error fetching links data:', error);
-  }
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data);
 }
 
-// Function to display activity links
-function displayLinks(weeks) {
-  const linksContainer = document.getElementById('#cards');
+const displayLinks = (weeks) => {
+  weeks.foreach((week) => {
+    let card = document.createElement("section");
+    let week = document.createElement("p");
+    let activity = document.createElement("a");
 
-  // Clear existing content
-  linksContainer.innerHTML = '';
- // Loop through weeks
-  weeks.forEach(week => {
-      const weekHeading = document.createElement('h3');
-      weekHeading.textContent = `Week ${week.lesson}`;
+    week.textcontent = `${week.lesson}`;
+    activity.setAttribute("href", week.url);
+    activity.setAttribute("");
 
-      const linksList = document.createElement('ul');
+    card.appendChild(week);
+    card.appendChild(activity);
 
-      // Loop through links
-      week.links.forEach(link => {
-          const listItem = document.createElement('li');
-          const linkAnchor = document.createElement('a');
-          linkAnchor.href = baseURL + link.url;
-          linkAnchor.textContent = link.title;
-          listItem.appendChild(linkAnchor);
-          linksList.appendChild(listItem);
-      });
-
-      // Append week heading and links list to container
-      linksContainer.appendChild(weekHeading);
-      linksContainer.appendChild(linksList);
+    cards.appendChild(card);
   });
-}
-
-// Call getLinks function to start fetching and displaying links
-getLinks()
+};
+getLinks();
