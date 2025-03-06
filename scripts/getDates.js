2df2ea7e0e4032b47ff0be85b3bbd2ea8860e43c
byui-titus <titusrayz33@gmail.com>
2025-03-06 13:54:17 +0300

    hp

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,4 +1,10 @@
+// Dynamically set year
+const yearSpan = document.querySelector('#year');
+yearSpan.textContent = new Date().getFullYear();
 
+// Dynamically set last modified date
+const lastModified = document.querySelector('#lastModified');
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
 
 
 
