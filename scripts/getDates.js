a02b55d7350675f8234ab26159eea696978398e2
byui-titus <titusrayz33@gmail.com>
2025-03-12 21:07:22 +0300

    ho02

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,6 +7,36 @@ const lastModified = document.querySelector('#lastModified');
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
 
 
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("✪")) {
+		body.style.background = "#000";
+		body.style.color = "#fff";
+		modeButton.textContent = "💡";
+	} else {
+		body.style.background = "#aea4e3";
+		body.style.color = "#000";
+		modeButton.textContent = "✪";
+	}
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+	const hamburgerBtn = document.getElementById('menu');
+	const mainMenu = document.querySelector('.navigation');
+  
+	hamburgerBtn.addEventListener('click', function () {
+		mainMenu.style.display = 
+		(mainMenu.style.display === 'none' || mainMenu.style.display === 'block') ? 
+		'' : 'block';
+	});
+	hamburger.addEventListener('click', () => {
+		menu.classList.toggle('show');
+		hamburger.innerHTML = menu.classList.contains('show') ? 'X' : '&#9776;';
+	});
+  });
+
 
 
 
